f4cfbf60569015034a34d3f500bad22a587b12c2
Kathycat78 <Kathy@78>
2025-10-02 14:50:46 +0200

    20-modification-du-commit-si-tu-es-le-créateur

--- public/index.php
+++ public/index.php
@@ -1,14 +1,32 @@
 <?php
+
 require_once(__DIR__ . '/../vendor/autoload.php');
+
 session_start();
+
 use Config\Router;
+
+
 
 $router = new Router;
 
+
+
 $router->addRoute('/', 'HomeController', 'index');
+
 $router->addRoute('/inscription', 'RegisterController', 'index');
+
 $router->addRoute('/connexion', 'SessionController', 'login');
+
 $router->addRoute('/deconnexion', 'SessionController', 'logout');
+
 $router->addRoute('/ajoutCommit', 'CommitController', 'addCommit');
+
+$router->addRoute('/commit', 'CommitController', 'commit');
+
+
+$router->addRoute('/modifier', 'CommitController', 'editCommit');
+
+
 
 $router->handleRequest();
